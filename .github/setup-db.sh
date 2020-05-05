1ee52ba0618ad18418c9236056eba4347f46ccee
Soner Sayakci <s.sayakci@shopware.com>
2020-05-05 21:18:39 +0200

    Update workflows

--- .github/setup-db.sh
+++ .github/setup-db.sh
@@ -6,11 +6,11 @@ set -v
 STARTPARAMS=""
 DB=$1
 
-if [[ $DB == "mysql:8.0" ]]; then
+if [[ $DB == "mysql:8.0.19" ]]; then
     STARTPARAMS="mysqld --default-authentication-plugin=mysql_native_password"
 fi
 
-docker run -it --mount type=tmpfs,destination=/var/lib/mysql --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_USER=travis -e MYSQL_PASSWORD=travis -e MYSQL_DATABASE=shopware -p3306:3306 ${DB} ${STARTPARAMS}
+docker run -it --mount type=tmpfs,destination=/var/lib/mysql --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_USER=shopware -e MYSQL_PASSWORD=shopware -e MYSQL_DATABASE=shopware -p3306:3306 ${DB} ${STARTPARAMS}
 sleep 5
 
 mysql() {
--- .github/setup-es.sh
+++ .github/setup-es.sh
@@ -1,6 +1,4 @@
-if [[ -z ${ELASTICSEARCH} ]]; then
-    exit 0
-fi
+ELASTICSEARCH=$1
 
 docker pull ${ELASTICSEARCH}
 docker run -it --name=elasticsearch -e "discovery.type=single-node" -d -p 9200:9200 ${ELASTICSEARCH}
